a9193950f577d21cd7884b86ed47f5f41019bfbb
isayu <isayu1571@gmail.com>
2025-12-22 20:42:22 +0800

    login註解改好了 耶耶
    剩下收藏

--- page5.js
+++ page5.js
@@ -1,4 +1,4 @@
-
+// 滑鼠
 const canvas = document.getElementById('cursorCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -15,29 +15,22 @@ window.addEventListener('mousemove', (e) => {
     mouseY = e.clientY;
 });
 
-// 監聽滑鼠按下/放開
+
 window.addEventListener('mousedown', () => mouseDown = true);
 window.addEventListener('mouseup', () => mouseDown = false);
 
 function drawCursor() {
-    // 清空畫布
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     ctx.save();
     ctx.translate(mouseX, mouseY);
-
-    // 旋轉箭頭方向 (-45度 = 左上)
     ctx.rotate(-Math.PI / 4);
-
-    // 顏色設定
     ctx.fillStyle = mouseDown ? '#F3E9EB' : '#F2285A';
 
-    // 箭頭形狀
     ctx.beginPath();
-    ctx.moveTo(0, -15);    // 上尖
-    ctx.lineTo(10, 10);    // 右下
-    ctx.lineTo(0, 5);      // 中下
-    ctx.lineTo(-10, 10);   // 左下
+    ctx.moveTo(0, -15);    
+    ctx.lineTo(10, 10);    
+    ctx.lineTo(0, 5);      
+    ctx.lineTo(-10, 10);   
     ctx.closePath();
     ctx.fill();
 
@@ -72,7 +65,7 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 document.querySelector(".form").addEventListener("submit", async (event) => {
-  event.preventDefault();
+  event.preventDefault();// 防止表單自動提交
 
   const usernameInput = document.getElementById("username").value.trim();
   if (!usernameInput) {
@@ -84,15 +77,13 @@ document.querySelector(".form").addEventListener("submit", async (event) => {
     try {
       let currentUser = user;
 
-      // 如果還沒登入，才匿名登入
       if (!currentUser) {
         const userCredential = await signInAnonymously(auth);
         currentUser = userCredential.user;
       }
-
       const uid = currentUser.uid;
 
-      // 存 username（同 uid 會覆寫，不會長垃圾資料）
+      // 存 username
       await setDoc(doc(db, "users", uid), {
         username: usernameInput
       }, { merge: true });
